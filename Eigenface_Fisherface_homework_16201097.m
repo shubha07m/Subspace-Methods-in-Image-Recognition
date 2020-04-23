8ea63a3efda3e8f8712df9ad973b4f034e43ea46
Shubhabrata Mukherjee <shubha07m@gmail.com>
2020-04-23 10:02:44 -0500

    Updated version with added new images

--- Eigenface_Fisherface_homework_16201097.m
+++ Eigenface_Fisherface_homework_16201097.m
@@ -1,19 +1,35 @@
-load facedata % Loading the data source (1)
+clear all;
+clc;
+load facedata % Loading the original data set (1)
 
-%% Computing PCA (2)
+%% Converting new images to a new matrix similar to original data set
+%% Appending the new data with the original data set (2)
+newfaces = [];
+for i = 1:10
+x = imread(['el',num2str(i),'.jpg']);
+x = rgb2gray(x)
+x = imresize(x,[1,400])
+newfaces =[newfaces;x];
+end
+newfaces = rescale(newfaces,0,1);
+newids = 999*ones(10,1);
+faces = [faces; newfaces];
+ids = [ids; newids];
+
+%% Computing PCA (3)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-[A1, s, lat]=pca(faces); 
+[A1, s, lat]=pca(faces);
 
 % dimension of each figure
 h=20; w=20;
 
-%% Plotting PCA component as per significance and energy (3)
+%% Plotting PCA component as per significance and energy (4)
 figure(1); 
 subplot(1,2,1); grid on; hold on; stem(lat, '.');title('Principle component significance') 
 f_eng=lat.*lat; 
 subplot(1,2,2); grid on; hold on; plot(cumsum(f_eng)/sum(f_eng), '.-'); title('PCA energy distribution')
 
-%% Plotting Eigen Faces (4)
+%% Plotting Eigen Faces (5)
 
 figure(2)
 for k=1:10
@@ -21,7 +37,7 @@ for k=1:10
     title(sprintf('EigenFace:%d', k));
 end
 
-%% top 16 feature selected & shown feature distances between 200 images (5)
+%% top 16 feature selected & shown feature distances between 200 images (6)
 kd=16; nface=200;
 
 x=faces*A1(:, 1:kd); 
@@ -29,26 +45,26 @@ f_dist = pdist2(x(1:nface,:), x(1:nface,:));
 figure(3); 
 imagesc(f_dist); colormap('gray'), title('Feature distance between 200 images');
 
-%% calculating true positive, false positive, true negative, false negative (6)
+%% calculating true positive, false positive, true negative, false negative (7)
 %% upto 7 matching pair , rest are non matching
 
 d0 = f_dist(1:7,1:7); d1=f_dist(8:end, 8:end);
 [tp, fp, tn, fn]= getPrecisionRecall(d0(:), d1(:), 40);
 
-%% plotting the ROC curve for EigenFace detection (7)
+%% plotting the ROC curve for EigenFace detection (8)
 figure(4); hold  on; grid on;
 plot(fp./(tn+fp), tp./(tp+fn), '.-r', 'DisplayName', 'tpr-fpr color, data set 1');
 xlabel('fpr'); ylabel('tpr'); title('EigenFace recognition performance for 16 features');
 legend('kd=16');
 
-%% Plotting of image point in 3D feature space Eigenface separation in feature space (8 & 9)
+%% Plotting of image point in 3D feature space Eigenface separation in feature space (9 & 10)
 figure(5); hold on; grid on; 
 styl = ['*r'; 'ob'; '+k'; '^m'];
 for k=1:4
     figure(5); offs = find(ids==k); plot3(x(offs, 1), x(offs,2), x(offs, 3), styl(k,:) );title('Eigenface separation in feature space');
     figure(6); subplot(2,2,k); imagesc(reshape(faces(offs(1),:), [h, w])); colormap('gray');title(sprintf('EigenFace person:%d', k));
 end
-%% End of Eigenface implementation (10)%%
+%% End of Eigenface implementation (11)%%
 %%%%%%%%%%%%%%%%%%%%%%%%%
 %%
 
